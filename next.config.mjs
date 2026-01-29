8e8a43cd660c8155ac484cb7c3439d548dcd2e80
dpvtesting <dpvformemail@gmail.com>
2026-01-29 10:24:55 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-      
-        new URL('https://admin.digitalplace.ai/storage/uploads/**')
-      
+      {
+        protocol: 'https',
+        hostname: 'admin.digitalplace.ai',
+      },
     ],
+  },
+  // THIS IS THE MAGIC PART
+  async rewrites() {
+    return [
+      {
+        source: '/storage/uploads/:path*',
+        destination: 'https://admin.digitalplace.ai/storage/uploads/:path*',
+      },
+    ];
   },
 };
 
 export default nextConfig;
-
-
-
-
-

5b816a859af5e26a71be7dddbf45f0994454a4e6
dpvtesting <dpvformemail@gmail.com>
2026-01-29 08:58:52 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [new URL('https://admindpv.newman.id/storage/uploads/**')],
     unoptimized: true
+    protocol: 'https',
+    hostname: 'admin.digitalplace.ai',
   },
 };
 
 export default nextConfig;
+
